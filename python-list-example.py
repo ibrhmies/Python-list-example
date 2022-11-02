002deee38ec7cea3051271db4cf07c8c9024fe5c
ibrhmies <02220201044@ogr.inonu.edu.tr>
2022-11-03 01:01:47 +0300

    Python-list-example
    Python'da liste metotlarıyla uygulama

---
+++ python-list-example.py
@@ -0,0 +1,14 @@
+#-> kullanıcıdan alacağınız 3 tane marka bilgisini marka adı altında listeleyiniz
+
+markalar = []
+
+marka = input("Marka: ")
+markalar.append(marka)
+
+marka = input("Marka: ")
+markalar.append(marka)
+
+marka = input("Marka: ")
+markalar.append(marka)
+
+print(markalar)
